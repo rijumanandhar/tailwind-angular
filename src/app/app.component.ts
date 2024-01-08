205148ef075ed88d28224a19e9aa27a83fac7814
Riju Manandhar <rijumd@gmail.com>
2024-01-09 00:56:04 +0545

    added event listener to card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwind-angular';
+  card1view = true;
+  card2view = false;
+  card3view = false;
+  card4view = false;
+
+  public resetCards(){
+    this.card1view = false;
+    this.card2view = false;
+    this.card3view = false;
+    this.card4view = false;
+  }
+
+  public clickCard1(){
+    this.resetCards();
+    this.card1view = true;
+  } 
 }
